24a3adb0b196a086a69eba8654a27fe9e0ea3e20
sahasagnik022-cmyk <sahasagnik022@gmail.com>
2026-07-20 19:20:34 +0530

    Update apb_generator.sv

--- testbench/apb_generator.sv
+++ testbench/apb_generator.sv
@@ -3,7 +3,7 @@ class apb_generator;
   function new(mailbox #(apb_transaction) mbx);
     this.mbx = mbx;
   endfunction
-  
+
   task run_b2b_write(int num);
     write_tx wr_tx;
     $display("\n[GEN] Back to Back Write Test: %0d ---", num);
@@ -27,28 +27,29 @@ class apb_generator;
   endtask
 
   // Feature ID 10, 11
-  task run_write_read();
+  task run_write_read(int count=5);
     write_tx wr_tx;
     read_tx rd_tx;
     $display("\n[GEN] Write followed by Read");
-    
+    for(int i=0;i<count;i++) begin
     wr_tx = new();
     if(!wr_tx.randomize()) $fatal(1,"Randomization failed");
     mbx.put(wr_tx);
     $display("[%0t] [GEN] Write | Addr=%0d | Data=%0d | PSTRB=%0b", $time,wr_tx.PADDR,wr_tx.PWDATA,wr_tx.PSTRB);
-    
+
     rd_tx = new();
     if(!rd_tx.randomize()) $fatal(1,"Randomization failed");
     rd_tx.PADDR = wr_tx.PADDR;
     mbx.put(rd_tx);
     $display("[%0t] [GEN] Read | from Addr=%0d",$time,rd_tx.PADDR);
+    end
   endtask
-  
+
   // Feature 6
   task run_byte_test();
     byte_sel_tx byte_tx;
     $display("\n[GEN] Byte Select Test ");
-    for(int i=0; i<4; i++) begin
+    for(int i=0; i<50; i++) begin
         byte_tx = new();
         if(!byte_tx.randomize()) $fatal(1,"Randomization failed");
         mbx.put(byte_tx);
@@ -75,17 +76,30 @@ class apb_generator;
     mbx.put(nb_tx);
     $display("[%0t] [GEN] WRITE | Addr=%0d | Strobe=%0b", $time, nb_tx.PADDR, nb_tx.PSTRB);
   endtask
-  
+
   // Feature 14
   task run_error_test();
     error_tx err_tx;
     $display("\n[GEN] Error Check (Out of Bounds)");
-    for(int i=0;i<3; i++) begin
+    for(int i=0;i<10; i++) begin
       err_tx = new();
       if(!err_tx.randomize()) $fatal(1, "Randomization failed");
       mbx.put(err_tx);
       $display("[%0t] [GEN] Tx %0d | Addr=%0d (Expected Error)", $time,i, err_tx.PADDR);
     end
   endtask
-  
+
+  // MAIN TASK
+  task run();
+    $display("[GEN] STARTING FULL TEST");
+    run_b2b_write(50);
+    run_byte_test();
+    run_full_byte_test();
+    run_no_byte_test();
+    run_write_read(50);
+    run_b2b_read(50);
+    run_error_test();
+    $display("[GEN] FULL TEST COMPLETED");
+  endtask
+
 endclass
